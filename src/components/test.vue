db543d773dd6c77a09fbd7051d8573bf04574ace
Olzhas <oljasalidarov@gmail.com>
2022-02-28 21:04:05 +0600

    edits

--- src/components/test.vue
+++ src/components/test.vue
@@ -3,6 +3,11 @@
         <h1 v-if="status === 1">{{ transfer }}</h1>
         <p v-else>{{ heading }}</p>
         <button @click="changeStatus()">Click me!</button>
+        <div class="flex" v-for="item of arr" :key="item.id">
+            <p>{{ item.id }}.</p>
+            <h1>{{ item.surname }} {{ item.name }}</h1>
+        </div>
+        <img :src="heading" alt="">
     </div>
 </template>
 
@@ -12,7 +17,19 @@ export default {
     data () {
         return {
             heading: 'Hello Vue!',
-            status: 1
+            status: 1,
+            arr: [
+                {
+                    id: 1,
+                    name: 'Mansur',
+                    surname: 'Yolkin'
+                },
+                {
+                    id: 2,
+                    name: 'Batyrkhan',
+                    surname: 'Baurzhanuly'
+                }
+            ]
         }
     },
     props: [ 'transfer' ],
