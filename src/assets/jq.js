20dc98923e9b4537607ab54cf30ccf59ec5241e5
Olzhas <oljasalidarov@gmail.com>
2022-03-04 21:35:15 +0600

    bullet

--- src/assets/jq.js
+++ src/assets/jq.js
@@ -2,25 +2,63 @@ import $ from 'jquery';
 
 $(document).ready(function () {
 
-    var position = $('.ship').position();
+    var posX = 800;
+    var posY = 700;
+    $(".ship").css("left", posX);
+    $(".ship").css("top", posY);
+
     window.addEventListener('keydown', function (event) {
-        var x = 1;
+        console.log(posX, posY);
         if (event.keyCode == 65) {
             $('.ship').addClass('left');
-            $('.ship').position();
-            console.log( "left: " + position.left + ", top: " + position.top );
+            posX = posX - 25;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
         } else if (event.keyCode == 87) {
             $('.ship').addClass('top');
+            posY = posY - 25;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
         } else if (event.keyCode == 83) {
             $('.ship').addClass('bottom');
+            posY = posY + 25;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
         } else if (event.keyCode == 68) {
             $('.ship').addClass('right');
+            posX = posX + 25;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
         }
-        if ($('.ship').is('.top', '.left')) {
-            console.log('djhskalda')
+        if ($('.ship').hasClass("top") && $(".ship").hasClass("left")) {
             $('.ship').css('transform', 'rotate(-45deg)');
+            posX = posX - 45 / 1.4;
+            posY = posY - 25 / 1.4;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
+        } else if ($('.ship').hasClass("top") && $(".ship").hasClass("right")) {
+            $('.ship').css('transform', 'rotate(45deg)');
+            posX = posX + 45 / 1.4;
+            posY = posY - 25 / 1.4;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
+        } else if ($('.ship').hasClass("bottom") && $(".ship").hasClass("left")) {
+            $('.ship').css('transform', 'rotate(225deg)');
+            posX = posX - 45 / 1.4;
+            posY = posY + 25 / 1.4;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
+        } else if ($('.ship').hasClass("bottom") && $(".ship").hasClass("right")) {
+            $('.ship').css('transform', 'rotate(135deg)');
+            posX = posX + 45 / 1.4;
+            posY = posY + 25 / 1.4;
+            $(".ship").css("left", posX);
+            $(".ship").css("top", posY);
         };
     });
+    $('html').on('click', function () {
+        $(this).append('<img class="bullet" src="/img/bullet.5351763d.png" style="position: absolute; left: ' + posX + 'px' + '; top: ' + posY + 'px;">')
+    })
     window.addEventListener('keyup', function () {
         if (event.keyCode == 65) {
             $('.ship').removeClass('left');
