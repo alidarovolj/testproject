f58a1c1f8a9cc63aa14c87b747b55a58ecadf2c9
Olzhas <oljasalidarov@gmail.com>
2022-03-10 19:32:40 +0600

    djklsa

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        
+  <div class="carousel relative">
+    <div class="container mx-auto">
+      <div class="inner_content flex items-center">
+        <div class="slide w-full h-full">
+          <img class="w-full" src="../assets/1.jpg" alt="" />
+        </div>
+        <div class="slide w-full h-full">
+          <img class="w-full" src="../assets/2.jpg" alt="" />
+        </div>
+        <div class="slide w-full h-full">
+          <img class="w-full" src="../assets/3.jpg" alt="" />
+        </div>
+      </div>
+      <p class="arrow_first">&lt;</p>
+      <p class="arrow_second">&gt;</p>
     </div>
+  </div>
 </template>
