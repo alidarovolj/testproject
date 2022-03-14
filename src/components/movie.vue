7ed078a7ba385468ee91c2c7af32fea7cba615a3
Olzhas <oljasalidarov@gmail.com>
2022-03-14 19:17:17 +0600

    router

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -1,22 +1,15 @@
 <template>
-  <div>
-    <div v-for="movie of movies" :key="movie.id">
-      <p @click="$router.push({ name: 'Movie', params: { id: movie.id } })">
-        {{ movie.name }}
-      </p>
-      <p>{{ movie.desc }}</p>
-    </div>
-  </div>
+  <div>{{ movie.id }}</div>
 </template>
 
 <script>
 export default {
-  name: "Movies",
   data() {
     return {
       errors: [],
       movies: [],
       urlMovie: "http://localhost:3001/movies",
+      movie: null,
     };
   },
   async mounted() {
@@ -27,6 +20,14 @@ export default {
       console.log(this.movies);
     } catch (error) {
       this.errors.push(error);
+    }
+  },
+  created() {
+    const movie = this.movies.find(
+      (movie) => movie.id == this.$route.params.id
+    );
+    if (movie) {
+      this.movie = movie;
     }
   },
 };
