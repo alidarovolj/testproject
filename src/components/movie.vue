a2c77efe10b75fe8102804742a3bd75baf6e9f20
Olzhas <oljasalidarov@gmail.com>
2022-03-14 20:22:10 +0600

    router

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -1,5 +1,61 @@
 <template>
-  <div>{{ movie.id }}</div>
+  <div class="bg-black text-white">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-center">
+        <div class="block">
+          <h1 class="text-4xl font-semibold">{{ movies[curID].name }}</h1>
+          <div class="flex border-t border-b border-gray-700 py-5 mt-5">
+            <div>
+              <h2 class="uppercase text-xl mb-3">Metascore</h2>
+              <p class="w-3/5">Universal acclaim based on 16 Critic Reviews</p>
+            </div>
+            <div class="flex items-center">
+              <img v-if="movies[curID].mustSee"
+                class="w-16 mx-3"
+                src="https://www.metacritic.com/images/icons/mc-mustsee.svg"
+                alt=""
+              />
+              <p
+                class="
+                  mr-0
+                  text-3xl
+                  font-bold
+                  px-1
+                  py-3
+                  bg-green-500
+                  rounded-md
+                "
+              >
+                {{ movies[curID].meta }}
+              </p>
+            </div>
+          </div>
+          <div class="flex justify-between items-start border-b border-gray-700 py-5">
+            <div>
+              <h2 class="uppercase text-xl mb-3">User Score</h2>
+              <p class="w-3/5">Universal acclaim based on 16 Critic Reviews</p>
+            </div>
+            <div class="flex items-center">
+              <p
+                class="
+                  mr-0
+                  text-3xl
+                  font-bold
+                  px-1
+                  py-3
+                  bg-green-500
+                  rounded-md
+                "
+              >
+                {{ movies[curID].user }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <video muted autoplay :src="movies[curID].video"></video>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,9 +66,10 @@ export default {
       movies: [],
       urlMovie: "http://localhost:3001/movies",
       movie: null,
+      curID: this.$route.params.id - 1,
     };
   },
-  async mounted() {
+  async created() {
     try {
       const takeData = await fetch(this.urlMovie);
       const result = await takeData.json();
@@ -22,7 +79,7 @@ export default {
       this.errors.push(error);
     }
   },
-  created() {
+  mounted() {
     const movie = this.movies.find(
       (movie) => movie.id == this.$route.params.id
     );
--- src/components/movies.vue
+++ src/components/movies.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <div v-for="movie of movies" :key="movie.id">
-      <p @click="$router.push({ name: 'Movie', params: { id: movie.id } })">
-        {{ movie.name }}
-      </p>
-      <p>{{ movie.desc }}</p>
+  <div class="container mx-auto">
+    <div class="py-8 border-t border-b border-gray-400" v-for="movie of movies" :key="movie.id">
+      <div class="flex items-start">
+        <img class="mr-4" :src="movie.image" alt="" />
+        <div class="info w-full">
+          <p class="font-bold mb-3" @click="$router.push({ name: 'Movie', params: { id: movie.id } })">
+            <span>{{ movie.id }}</span> {{ movie.name }}
+          </p>
+          <p class="mb-3" style="color: #999">{{ movie.date }}</p>
+          <p class="w-full">{{ movie.desc }}</p>
+        </div>
+        <p class="mr-0 text-3xl font-bold px-1 py-3 bg-green-500 rounded-md">{{ movie.meta }}</p>
+      </div>
     </div>
   </div>
 </template>
