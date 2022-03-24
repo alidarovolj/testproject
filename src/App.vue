d022497a7323870c6e77d3600d4a1dcd4bb3ffdd
Olzhas <oljasalidarov@gmail.com>
2022-03-24 20:20:46 +0600

    css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import navbar from './components/Navbar.vue'
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    navbar
+  }
 }
 </script>
